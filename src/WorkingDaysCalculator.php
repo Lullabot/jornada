9fdf45530b68069c431a1877697ce93fdab55970
Andrew Berry <andrew.berry@lullabot.com>
2020-08-10 10:39:51 -0400

    Initial copy from project

---
+++ src/WorkingDaysCalculator.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lullabot\Jornada;
+
+/**
+ * Calculates working days based on typical 5 day working weeks with holidays.
+ *
+ * PHP does not have support for "date only" date objects, so this class
+ * ignores all times included with the dates except for the purposes of shifting
+ * dates by time zones.
+ */
+class WorkingDaysCalculator {
+
+    /**
+     * @var \DateTimeInterface[]
+     */
+    private $holidays = [];
+
+    /**
+     * Set holiday days, in addition to weekends.
+     *
+     * @param \DateTimeInterface[] $holidays
+     */
+    public function setHolidays(array $holidays): void
+    {
+        $this->holidays = $holidays;
+    }
+
+    /**
+     * Add a new holiday date.
+     *
+     * @param \DateTimeInterface $dateTime
+     */
+    public function addHoliday(\DateTimeInterface $dateTime) {
+        $this->holidays[] = $dateTime;
+    }
+
+    /**
+     * Get the working days between two dates, including those days.
+     *
+     * @param \DateTimeInterface $startDate
+     * @param \DateTimeInterface $endDate
+     *
+     * @return int
+     */
+    public function getWorkingDays(
+        \DateTimeInterface $startDate,
+        \DateTimeInterface $endDate
+    ): int {
+        if ($endDate < $startDate) {
+            throw new \InvalidArgumentException(sprintf('The end date must not be before the start date'));
+        }
+
+        // By default diff and intervals don't include the last date. Extend
+        // by one day so we are inclusive of the end date. We need to clone in
+        // the case that $endDate is a \DateTime and not \DateTimeImmutable.
+        $cloned = clone $endDate;
+        $cloned = $cloned->modify('+1 day');
+
+        // Include start and end, as we check them below for if they are working
+        // days.
+        $days = $startDate->diff($cloned)->d;
+
+        $period = new \DatePeriod(
+            $startDate, new \DateInterval('P1D'), $cloned
+        );
+        foreach ($period as $dt) {
+            if (!$this->isWorkingDay($dt)) {
+                $days--;
+            }
+        }
+        return $days;
+    }
+
+    /**
+     * Return if a given date is a working day.
+     *
+     * A working day is not a weekend (M-F) and not a holiday.
+     *
+     * @param \DateTimeInterface $date
+     *
+     * @return bool
+     */
+    public function isWorkingDay(\DateTimeInterface $date): bool
+    {
+        return !$this->isWeekend($date) && !$this->isHoliday($date);
+    }
+
+    /**
+     * Return if a given date is on the weekend.
+     *
+     * @param \DateTimeInterface $date
+     *
+     * @return bool
+     */
+    public function isWeekend(\DateTimeInterface $date): bool {
+        return in_array($date->format('D'), ['Sat', 'Sun']);
+    }
+
+    /**
+     * Return if a given date is a holiday.
+     *
+     * Holidays change yearly per region, so they must be set by calling one of
+     * the holiday methods.
+     *
+     * @param \DateTimeInterface $date
+     *
+     * @return bool
+     */
+    public function isHoliday(\DateTimeInterface $date): bool
+    {
+        if (!$this->holidays) {
+            return false;
+        }
+
+        $holidays = array_map(function(\DateTimeInterface $value) {
+            return $value->format('Y-m-d');
+        }, $this->holidays);
+
+        return (in_array($date->format('Y-m-d'), $holidays));
+    }
+
+    /**
+     * Add the given number of working days and return a new date.
+     *
+     * @param \DateTimeInterface $date
+     * @param int $days
+     *
+     * @return \DateTimeImmutable
+     */
+    public function addDays(\DateTimeInterface $date, int $days): \DateTimeImmutable
+    {
+        $daysAdded = clone $date;
+        if ($date instanceof \DateTime) {
+            $daysAdded = \DateTimeImmutable::createFromMutable($date);
+        }
+
+        for ($i = 0; $i < $days; $i++) {
+            $daysAdded = $daysAdded->modify('+1 day');
+
+            // Push ahead for any holidays and weekends.
+            while (!$this->isWorkingDay($daysAdded)) {
+                $daysAdded = $daysAdded->modify('+1 day');
+            }
+        }
+
+        return $daysAdded;
+    }
+
+}
---
+++ tests/src/Unit/WorkingDaysCalculatorTest.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lullabot\Jornada\Tests\Unit;
+
+use Lullabot\Jornada\WorkingDaysCalculator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Lullabot\Jornada\WorkingDaysCalculator
+ */
+class WorkingDaysCalculatorTest extends TestCase {
+
+    public function testGetWorkingDays() {
+        // Test 5 days in a week.
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-12');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(5, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testGetWorkingDaysImmutable() {
+        // Test 5 days in a week.
+        $monday = \DateTimeImmutable::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTimeImmutable::createFromFormat('Y-m-d', '2020-06-12');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(5, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testGetWorkingDaysTwoWeeks()
+    {
+        // Test 10 days in two weeks.
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-19');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(10, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testOneWorkingDay()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(1, $calculator->getWorkingDays($monday, $monday));
+    }
+
+    public function testTwoWorkingDays()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $tuesday = \DateTime::createFromFormat('Y-m-d', '2020-06-09');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(2, $calculator->getWorkingDays($monday, $tuesday));
+    }
+
+    public function testZeroWorkingDays()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $calculator = new WorkingDaysCalculator();
+        $calculator->addHoliday($monday);
+        $this->assertEquals(0, $calculator->getWorkingDays($monday, $monday));
+    }
+
+    public function testNotMondayStart()
+    {
+        $tuesday = \DateTime::createFromFormat('Y-m-d', '2020-07-21');
+        $nextTuesday = \DateTime::createFromFormat('Y-m-d', '2020-07-28');
+        $nextMonday = \DateTime::createFromFormat('Y-m-d', '2020-07-27');
+        $calculator = new WorkingDaysCalculator();
+        $this->assertEquals(5, $calculator->getWorkingDays($tuesday, $nextMonday));
+        $this->assertEquals(6, $calculator->getWorkingDays($tuesday, $nextTuesday));
+    }
+
+    public function testHasHoliday()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-12');
+        $calculator = new WorkingDaysCalculator();
+        $calculator->setHolidays([$monday]);
+        $this->assertEquals(4, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testMultipleHolidays()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-12');
+        $calculator = new WorkingDaysCalculator();
+        $calculator->setHolidays([$monday, $friday]);
+        $this->assertEquals(3, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testHasWeekendHoliday()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-19');
+        $calculator = new WorkingDaysCalculator();
+        $calculator->addHoliday(\DateTime::createFromFormat('Y-m-d', '2020-06-13'));
+        $this->assertEquals(10, $calculator->getWorkingDays($monday, $friday));
+    }
+
+    public function testEndBeforeStart()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $friday = \DateTime::createFromFormat('Y-m-d', '2020-06-12');
+        $calculator = new WorkingDaysCalculator();
+        $this->expectException(\InvalidArgumentException::class);
+        $calculator->getWorkingDays($friday, $monday);
+    }
+
+    public function testAddDays()
+    {
+        $monday = \DateTime::createFromFormat('Y-m-d', '2020-06-08');
+        $calculator = new WorkingDaysCalculator();
+        $calculator->addHoliday(\DateTimeImmutable::createFromFormat('Y-m-d', '2020-06-10'));
+        $addOne = $calculator->addDays($monday, 1);
+        $this->assertEquals('2020-06-09', $addOne->format('Y-m-d'));
+        $addTwo = $calculator->addDays($monday, 2);
+        $this->assertEquals('2020-06-11', $addTwo->format('Y-m-d'));
+        $addFive = $calculator->addDays($monday, 5);
+        $this->assertEquals('2020-06-16', $addFive->format('Y-m-d'));
+    }
+}
