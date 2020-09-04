ae762bd1c7a5a8b7cdbea50f1454f6cc1c58d8ce
Andrew Berry <andrew.berry@lullabot.com>
2020-09-04 14:58:45 -0400

    Add a class to calculate a whole team's availability

---
+++ src/TeamCalculator.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lullabot\Jornada;
+
+/**
+ * Calculate working days for a whole team.
+ *
+ * This class accepts one or more WorkingDaysCalculator objects, with the idea
+ * being that each represents an individual team member. That way, separate
+ * sets of PTO or regional holidays can be accounted as they vary across the
+ * whole team.
+ */
+class TeamCalculator
+{
+    /**
+     * @var \Lullabot\Jornada\WorkingDaysCalculator[]
+     */
+    private $calculators = [];
+
+    /**
+     * Add a calculator to the team.
+     *
+     * @param string                                  $id         A unique ID to reference the calculator by. To update an existing
+     *                                                            calculator, call this method again with the same ID.
+     * @param \Lullabot\Jornada\WorkingDaysCalculator $calculator the calculator to add
+     */
+    public function addCalculator(
+        string $id,
+        WorkingDaysCalculator $calculator
+    ): void {
+        $this->calculators[$id] = $calculator;
+    }
+
+    /**
+     * Return the number of working days for the whole team.
+     */
+    public function getWorkingDays(
+        \DateTimeInterface $startDate,
+        \DateTimeInterface $endDate
+    ): int {
+        $days = 0;
+        foreach ($this->calculators as $calculator) {
+            $days += $calculator->getWorkingDays($startDate, $endDate);
+        }
+
+        return $days;
+    }
+
+    /**
+     * Return the number of business days for the whole team.
+     */
+    public function getBusinessDays(
+        \DateTimeInterface $startDate,
+        \DateTimeInterface $endDate
+    ): int {
+        $days = 0;
+        foreach ($this->calculators as $calculator) {
+            $days += $calculator->getBusinessDays($startDate, $endDate);
+        }
+
+        return $days;
+    }
+}
